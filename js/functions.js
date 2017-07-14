3faf9fe886533d83b75baa1d9e3eb23fc84dcda5
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2017-07-13 21:19:50 -0400

    still working on the logo centering and moving animation
    the center logo moves here an there with the scroll movements and the
    resizing also takes care of the logo image movement,

--- js/functions.js
+++ js/functions.js
@@ -4,40 +4,63 @@ var wWin = $(window).width();
 var og_hLogo = $('.logo').height();
 var og_wLogo = $('.logo').width();
 var movelogo = true;
+var tLogo = hWin/2 ;  
+var lLogo = wWin/2 ;
 
+console.log(hWin)
+console.log(wWin)
+console.log(tLogo)
+console.log(lLogo)
+
+$( window ).resize(function() {
+    
+    hWin = $(window).height();
+    wWin = $(window).width();
+    tLogo = hWin/2 ;  
+    lLogo = wWin/2 ;
+
+    if($('.logo').top > 200){
+        $('.logo').animate({
+            top: hWin/2,
+            left: wWin/2,
+            width: og_wLogo,
+            height: og_hLogo
+        },1000);
+    }
+
+});
 
 $(window).scroll(function(){
 
     var wScroll = $(this).scrollTop();
-    console.log(wScroll)
+    //console.log(wScroll)
 
     //  ------ Moving Logo -------
 
 
-    var tLogo = hWin/2 - og_hLogo/2 ;  
-    var lLogo = wWin/2 - 100 ;
+    
     if(wScroll>200 && movelogo){
 
         // $('.logo').css({'position':'absolute'});
 
         $('.logo').animate({
             top: 150,
-            left: 50,
+            left: 150,
             width: ($('.logo').width()/3),
             height: ($('.logo').height()/3)
         },1000);
 
         $('.logo').css({'position':'fixed'});
         movelogo = false;
-        console.log('in if')
-        console.log(movelogo)
+        //console.log('in if')
+        //console.log(movelogo)
 
 
     }else if(wScroll<200 && !movelogo){
-        console.log('in else if')
+        //console.log('in else if')
 
-        console.log(tLogo)
-        console.log(lLogo)
+        //console.log(tLogo)
+        //console.log(lLogo)
      
 
         $('.logo').animate({
@@ -47,11 +70,16 @@ $(window).scroll(function(){
             height: og_hLogo
         },1000);
 
+        console.log(hWin)
+        console.log(wWin)
+        console.log(tLogo)
+        console.log(lLogo)
+
         $('.logo').css({'position':'fixed'});
         movelogo = true;
 
     }else{ 
-        console.log('in else')
+        //console.log('in else')
 
     }
 
